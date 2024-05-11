a04a42cf3d73d691267712c0c5d70b85a7784996
Joey Gallegos <joey@joeygallegos.com>
2024-05-11 00:13:22 -0500

    Enhancements to fetch command

--- run.py
+++ run.py
@@ -36,7 +36,10 @@ def fetch_emails(mail, start_date, end_date):
     conn, cursor = setup_db()
     result, data = mail.search(None, f'(SINCE "{start_date}" BEFORE "{end_date}")')
     uids = data[0].split()
-    for uid in uids:
+    num_emails_processed = 0
+    total_ingested_data = 0
+
+    for index, uid in enumerate(uids, start=1):
         result, data = mail.fetch(uid, "(RFC822)")
         raw_email = data[0][1]
         msg = email.message_from_bytes(raw_email)
@@ -50,6 +53,17 @@ def fetch_emails(mail, start_date, end_date):
             (uid, sender, subject, date),
         )
         conn.commit()
+        num_emails_processed += 1
+        total_ingested_data += len(raw_email)
+
+        if num_emails_processed % 25 == 0:
+            print(f"{num_emails_processed} emails processed")
+            sys.stdout.flush()  # Flush the output buffer
+
+    # Print final progress
+    print(f"All emails processed! Count: {num_emails_processed}")
+    sys.stdout.flush()  # Flush the output buffer
+
     conn.close()
 
 
@@ -121,7 +135,9 @@ def main():
     if len(sys.argv) > 1:
         command = sys.argv[1]
         if command == "fetch":
-            fetch_emails(mail, "01-Jan-2005", "31-Dec-2023")
+            start = config.get("FETCH", "START")
+            end = config.get("FETCH", "END")
+            fetch_emails(mail, start, end)
         elif command == "top":
             top_senders()
         elif command == "to_excel":
